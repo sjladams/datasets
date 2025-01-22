37cfea8a985aacbc47c491c2e504d6ee615929fb
sjladams <steven.adams.95@hotmail.com>
2025-01-22 09:26:03 +0100

    for 2d custom classification datasets, project data to in_feature dimensional space if in_features>2

--- src/datasets/loaders/custom_classification.py
+++ src/datasets/loaders/custom_classification.py
@@ -8,18 +8,54 @@ from datasets.core.templates import ClassificationDataset
 import datasets.core.utils as utils
 
 
+def get_projection_matrix(n: int, m: int, randomly: bool = True):
+    """
+    Generate a random projection matrix where columns are independent
+
+    Args:
+        n (int): Target dimensionality (number of rows in the matrix).
+        m (int): Number of columns in the projection matrix.
+        randomly (bool): If True, generate a random matrix. If False, generate a basis vector projection.
+    """
+    if randomly:
+        # Generate a random matrix
+        T = torch.randn(n, m)
+
+        # Ensure orthogonality by QR decomposition
+        T, _ = torch.linalg.qr(T)
+    else:
+        # Generate basis vector projection
+        T = torch.zeros(n, m)
+        indices = torch.randint(0, n, (m,))
+        T[indices, :] = torch.eye(m)
+    return T
+
+
+def project_data(dataset_name: str, data: torch.Tensor, in_features: int, basis_features: int, **kwargs):
+    proj_mat_path = f"{utils.get_local_data_root(dataset_name)}{os.sep}proj_mat_in={in_features}.txt.gz"
+    if os.path.exists(proj_mat_path):
+        proj_mat = utils.open_txt_gz(proj_mat_path, dtype=torch.float32)
+    else:
+        proj_mat = get_projection_matrix(in_features, basis_features, **kwargs)
+        utils.save_txt_gz(proj_mat_path, proj_mat)
+
+    return data @ proj_mat.t()
+
+
 def half_moons(len_dataset: int, in_features: int=2, noise=0.1):
-    if in_features != 2:
+    data, targets = make_moons(n_samples=len_dataset, noise=noise)
+    data, targets = torch.as_tensor(data, dtype=torch.float32), torch.as_tensor(targets, dtype=torch.int64)
+
+    # if in_features > 2, project the data into an in_features-dimensional space
+    if in_features > 2:
+        data = project_data("half_moons", data, in_features, 2, randomly=False)
+    else:
         raise ValueError
 
-    data, targets = make_moons(n_samples=len_dataset, noise=noise)
-    return torch.as_tensor(data, dtype=torch.float32), torch.as_tensor(targets, dtype=torch.int64)
+    return data, targets
 
 
 def vertical_split(len_dataset: int, in_features: int=2):
-    if in_features != 2:
-        raise ValueError
-
     # Generate random points in the range [0, 1] for both dimensions
     data = torch.rand(len_dataset, 2)
 
@@ -32,31 +68,44 @@ def vertical_split(len_dataset: int, in_features: int=2):
 
     targets = torch.cat([torch.zeros(len_dataset // 2, dtype=torch.int64),
                          torch.ones(len_dataset // 2, dtype=torch.int64)])
+
+    # if in_features > 2, project the data into an in_features-dimensional space
+    if in_features > 2:
+        data = project_data("vertical_split", data, in_features, 2, randomly=False)
+    else:
+        raise ValueError
+
     return data, targets
 
 
 def diagonal_split(len_dataset: int, in_features: int=2):
-    if in_features != 2:
-        raise ValueError
-
     # Generate random points in the range [-1, 1] for both dimensions
     data = torch.rand(len_dataset, 2) * 2 - 1
 
     # Assign class 0 to points below the diagonal y = x and class 1 to points above the diagonal
     targets = (data[:, 1] > -data[:, 0]).long()
 
+    # if in_features > 2, project the data into an in_features-dimensional space
+    if in_features > 2:
+        data = project_data("diagonal_split", data, in_features, 2, randomly=False)
+    elif in_features < 2:
+        raise ValueError
+
     return data, targets
 
 
 def ellipsoid_split(len_dataset: int, in_features: int=2, epsilon_x: float = 1.0, epsilon_y: float = 4.0):
-    if in_features != 2:
-        raise ValueError
-
     # Generate random points in the range [-1, 1] for both dimensions
     data = torch.rand(len_dataset, 2) * 2 - 1
 
     # Assign class 0 to points within the epsilon radius and class 1 to all other points
     targets = (data[:, 0].pow(2) * epsilon_x + data[:, 1].pow(2) * epsilon_y <= 0.8).long()
+
+    # if in_features > 2, project the data into an in_features-dimensional space
+    if in_features > 2:
+        data = project_data("ellipsoid_split", data, in_features, 2, randomly=False)
+    else:
+        raise ValueError
 
     return data, targets
 
